1fab2ff9698d19bf293e11bfc181a145497b6a5b
FábioTassinari <fabio.tassinari@smanager,tec,br>
2025-08-13 10:04:03 -0300

    First Commit

---
+++ run.sh
@@ -0,0 +1,11 @@
+REGISTRY=default-route-openshift-image-registry.apps.ocp-hml.ati.to.gov.br
+
+docker build -f src/main/docker/Dockerfile.jvm -t $REGISTRY/tassinari/quarkus-rest-test-jvm
+
+podman login -u admin  -p $(oc whoami -t) $REGISTRY --tls-verify=false
+
+podman push $REGISTRY/tassinari/quarkus-rest-test-jvm --tls-verify=false
+
+oc get is -n tassinari
+
+
